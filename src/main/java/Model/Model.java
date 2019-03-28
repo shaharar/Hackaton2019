3422596953a6f4758d65922b89360cbfb5ad7c86
einatkivity <einatkiv@post.bgu.ac.il>
2019-03-28 18:24:26 +0200

    insert

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Model {
@@ -19,6 +20,20 @@ public class Model {
             System.out.println(e.getStackTrace());
         }
         return conn;
+    }
+
+    private void insert(String name, String supply, int price, String category){
+        String sql = "INSERT INTO Users(USERNAME,PASSWORD,FIRSTNAME,LASTNAME,BIRTHDATE,CITY) VALUES(?,?,?,?,?,?)";
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, supply);
+            pstmt.setInt(3, price);
+            pstmt.setString(4, category);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("not goodddd");
+        }
     }
     
 }
