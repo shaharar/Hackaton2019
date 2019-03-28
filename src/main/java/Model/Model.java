b7ef6dd95e7a1e2bd15de3ea3e825e8db2ca8614
shaharar <shaharar@post.bgu.ac.il>
2019-03-28 18:26:09 +0200

    login

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -1,12 +1,18 @@
 package Model;
 
+import Controller.Controller;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Model {
+
+     Controller controller;
+
     //constructor
-    public Model() {
+    public Model(Controller controller) {
+        this.controller = controller;
     }
 
     private Connection connect() {
@@ -20,5 +26,17 @@ public class Model {
         }
         return conn;
     }
-    
+
+/*    public boolean login(String username, String password) {
+        if(DBM.ReadProfile(username)){ //if found in db
+            String realpass=DBM.getPassword(username);
+            if( realpass.equals(password)){
+                currentUser = DBM.getFields(username);
+                InitID();
+                return true;
+            }
+        }
+        return false;
+    }*/
+
 }
