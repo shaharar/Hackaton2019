9b0846df7aa8bf43cc4ad3194cc2fc238e97daf3
einatkivity <einatkiv@post.bgu.ac.il>
2019-03-29 09:59:12 +0300

    Merge branch 'master' of https://github.com/shaharar/Hackaton2019
    # Conflicts:
    #	src/main/java/Controller/Controller.java
    #	src/main/java/Model/Model.java
    #	src/main/java/View/Main.java

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -8,10 +8,10 @@ public class Controller {
     Model model;
     View.AView view;
 
-    public Controller(MainWebsiteView website) {
-        this.model = new Model(this);
-        this.view = website;
-    }
+//    public Controller(MainWebsiteView website) {
+//        this.model = new Model(this);
+//        this.view = website;
+//    }
 
 
     public boolean login(String username, String password) {
